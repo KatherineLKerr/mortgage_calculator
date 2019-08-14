469fbd1e07c9ff880cd50d5ccbf9a62a54ba9756
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-14 10:27:05 +0100

    basic mortgage form displays in container

--- src/components/MortgageForm.js
+++ src/components/MortgageForm.js
@@ -1,1 +1,23 @@
 import React, {Component} from 'react';
+
+class MortgageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      salary1: 0,
+      salary2: 0
+    }
+  }
+
+  render() {
+    return (
+      <form className="mortgage-form">
+        <input type="number" value={this.state.salary1}></input>
+        <input type="number" value={this.state.salary2}></input>
+        <input type="submit" value="Calculate Mortgage"></input>
+      </form>
+    )
+  }
+}
+
+export default MortgageForm;
--- src/containers/MortgageContainer.js
+++ src/containers/MortgageContainer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import AllowedMortgage from '../components/AllowedMortgage'
+import AllowedMortgage from '../components/AllowedMortgage';
+import MortgageForm from '../components/MortgageForm';
 
 class MortgageContainer extends Component {
 
@@ -16,6 +17,8 @@ class MortgageContainer extends Component {
       <div>
         <p>mortgage container </p>
         <AllowedMortgage mortgage1={this.state.mortgage1} mortgage2={this.state.mortgage2}/>
+        <p>mortgage form</p>
+        <MortgageForm />
       </div>
     )
   }
